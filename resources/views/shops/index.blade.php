3f7cc6e0e99be7ad48b9efe89a0ddd3d9c4c1350
Joshua Van Camp <joshuavancamp@gmail.com>
2021-01-21 14:51:27 -0500

    add app layout

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!-- CSS only -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <title>Shops</title>
-</head>
-<body>
-  <div class="container">
-    <h1 class='text-center my-5'> SHOPS Page </h1>
+@extends('layouts.app')
+@section('title')
+    <title>Shop Directory</title>
+@endsection
+@section('content')
+    <h1 class='text-center my-5'> Shop Directory </h1>
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card card-default">
@@ -27,6 +20,4 @@
         </div>
       </div>
     </div>
-  </div>
-</body>
-</html>
+@endsection
