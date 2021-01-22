4ad93117ad1a7fca82a6dbc06867ac2bd48ceaa8
Joshua Van Camp <joshuavancamp@gmail.com>
2021-01-22 10:29:33 -0500

    add cities

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(CitySeeder::class);
         $this->call(ShopSeeder::class);
     }
 }
